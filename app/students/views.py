eb8a90e38aecd4d39d49a4b4c8349d3f8dde4991
Noha Shehab <nohashehab.iti@gmail.com>
2023-03-10 09:44:49 +0200

    add data using POst method

--- app/students/views.py
+++ app/students/views.py
@@ -1,28 +1,6 @@
-from app.models import Student
-from flask import  render_template, redirect, url_for
+from app.models import Student,db
+from flask import  render_template, redirect, url_for, request
 
-# def testfunction():
-#     return '<h1> Students Index </h1>'
-#
-#
-# def students_index():
-#     students = Student.get_all_students()
-#     return render_template('students/index.html', students=students)
-#
-#
-#
-# def student_info(id):
-#     student = Student.query.get_or_404(id)
-#     return render_template('students/show.html', student=student)
-#
-#
-#
-# def student_delete(id):
-#     student = Student.query.get_or_404(id)
-#     res =student.delete_object()
-#     if res :
-#         return redirect(url_for("students_index"))
-#
 
 from app.students import  students_blueprint
 
@@ -49,4 +27,47 @@ def student_delete(id):
     res =student.delete_object()
     if res :
         return redirect(url_for("students.students_index"))
+
+def validateInputs(requestdict: dict):
+    errors = {}
+    if not requestdict["name"]:
+        errors["name"] = 'name required'
+
+    ## check email exists before
+    student = Student.query.filter_by(email=requestdict['email']).first()
+    if student:
+        errors["email"] = 'Email already exists'
+
+    return errors
+
+
+
+
+#### create
+@students_blueprint.route('/create',endpoint='create', methods=['GET','POST'])
+def createStudent():
+    if request.method == 'GET':
+        return render_template('students/create.html')
+    elif request.method == 'POST':
+        print(request.form)  # use request.form
+        requestdata = dict(request.form)
+        print(requestdata)
+        if not 'accepted' in requestdata:
+            requestdata['accepted']=False
+        else:
+            requestdata['accepted'] = True
+
+
+        request_errors = validateInputs(requestdata)
+        if request_errors:
+            return render_template('students/create.html',errors=request_errors)
+        else:
+            student = Student(**requestdata)
+            db.session.add(student)
+            db.session.commit()
+
+            return redirect(url_for('students.students_index'))
+
+
+
 
